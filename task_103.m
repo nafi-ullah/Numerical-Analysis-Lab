56f619ec8bd96227ebdad84066bb7c779665f9d4
nafi-ullah <shafinnafiullah@gmail.com>
2023-11-04 16:50:08 +0600

    task3 and images

--- task_103.m
+++ task_103.m
@@ -1,4 +1,4 @@
-function retval = task_1002 ()
+function retval = task_103 (input1, input2)
 
 function y = f(x)
     y = x.^2 - 4;
@@ -12,7 +12,7 @@ roots = [];
 iterations = 0;
 
 while (b - a) / 2 > t
-    c = (a + b) / 2;
+    c = (a*f(b) - b*f(a)) / (f(b) - f(a));
     roots = [roots; c];
 
     if f(c) == 0
@@ -28,19 +28,18 @@ end
 
 fprintf('%d iterations needed: %f\n', iterations, roots(end));
 
- x = linspace(0, 3, 100);
- y = f(x);
+x = linspace(0, 3, 100);
+y = f(x);
 
- figure;
- plot(x, y);
+figure;
+plot(x, y);
 hold on;
 plot(roots, f(roots), 'ro');
 xlabel('x');
-ylabel('function values');
-title('Bisection Method');
-legend('function values ', 'Roots');
+ylabel('function value');
+title('False-Position Method ');
+legend('function falues', 'Roots');
 grid on;
-
 
 
 endfunction
