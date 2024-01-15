d3ccbac58beba3699a4262dd8d9af81a4a7289d8
nafi-ullah <shafinnafiullah@gmail.com>
2024-01-15 18:53:05 +0600

    for any function input

--- SelfPrac/myscript.m
+++ SelfPrac/myscript.m
@@ -1,7 +1,16 @@
 function retval = myscript ()
-
+%{
   function y = f(x)
-    y = x.^2 - 4;
+    y = x.^2 - 4; % how to read this whole equation from user input
+  endfunction
+  %}
+
+  str = input('Enter equation :', 's');
+  f = inline(str,'x');
+
+  x = linspace(0,3,100);
+  function y = f(x);
+    y = f(x);
   endfunction
 
   a = 0;
@@ -27,10 +36,27 @@ function retval = myscript ()
   endwhile
 
   fprintf('%d iterations needed: %f\n', iterations, roots(end));
-
+%{
   for i = 1:length(roots)
     fprintf('%d no root: %d\n', i, roots(i));
   endfor
+  %}
+
+  x = linspace(0, 3, 100);
+  y= f(x);
+
+  figure;
+  plot(x,y);
+  hold on;
+
+  plot(roots, f(roots), 'or');
+  grid on;
+
+  xlabel('x');
+  ylabel('function value');
+
+  title('False postion method');
+  legend('function values','roots');
 
 
 endfunction
---
+++ SelfPrac/newton_raphsan.m
@@ -0,0 +1,5 @@
+
+
+function retval = newton_raphsan (input1, input2)
+
+endfunction
